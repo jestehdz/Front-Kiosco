d31e12a8b4b1153d3105eda579c6ddcbfed297a4
jestehdz <jestehdz@gmail.com>
2024-07-06 00:25:07 -0500

    api node

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRoute, Routes, Route} from 'react-router-dom';
+//import ShowProducts from './Components/ShowProducts';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRoute>
+    <Routes>
+      <Route path= '/' element = {<ShowProducts></ShowProducts>}></Route>
+    </Routes>
+    </BrowserRoute>
   );
 }
 
